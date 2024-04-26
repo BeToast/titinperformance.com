30bb1a73e713ed5538ff0750d6f031a85966e546
Blake Preston <note2bp@protonmail.com>
2024-04-25 20:05:02 -0600

    blackrect and sticky titin performance

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import "./App.css";
 import ShadowFrame from "./compos/ShadowFrame";
 import waitForElm from "./generic/waitForElm";
-import { TITIN } from "./svgs";
+import { TITIN, TITINperformance } from "./svgs";
 
 import { useEffect } from "react";
 
@@ -65,25 +65,73 @@ const positionLuke = (
    return lukeOffset;
 };
 //postition slogan relative to luke
-const positionSlogan = (slogan: HTMLElement, lukeOffset: DOMRect) => {
-   slogan.style.top = `${lukeOffset.top}px`;
+const positionSlogan = (
+   slogan: HTMLElement,
+   vw: number,
+   vh: number,
+   slantAglRad: number,
+) => {
+   const sloganOffset = slogan.getBoundingClientRect();
+   //solving for height of redslant at left of content area. so we can center slogan vertically
+   //adj of tanh function
+   const adj = vw * 0.5 - sloganOffset.left;
+   //solve for opposite side length with tanh.
+   const yFromVpCenterBottomOfRedSlant = -1 * slantAglRad * adj;
+
+   const redSlantHeightAtLeft = vh * 0.5 + yFromVpCenterBottomOfRedSlant;
+
+   slogan.style.top = `${redSlantHeightAtLeft * 0.5 - sloganOffset.height * 0.5}px`;
+
+   // slogan.style.top = `${lukeOffset.top + lukeOffset.height * 0.5 - sloganOffset.height * 0.5}px`;
+};
+
+const positionTitinPerform = (
+   titinPerformAr: HTMLElement[],
+   vh: number,
+   slantAglRad: number,
+) => {
+   //get height of text
+   const titinPerformOffset = titinPerformAr[2].getBoundingClientRect();
+   //set top value for sticky position to work, its the same space as text height
+   titinPerformAr[2].style.top = `${titinPerformOffset.height}px`;
+   //get additional whitespace above text to red using angle
+   const extraWhitespace = -1 * slantAglRad * titinPerformOffset.left;
+   //set height of above-titin-perform, concequetnly position the text.
+   const aboveTitinPerformHeight =
+      vh * 0.9 - titinPerformOffset.height * 0.5 - extraWhitespace * 0.5;
+   titinPerformAr[1].style.height = `${aboveTitinPerformHeight}px`;
+
+   //calc additional height to take up in black-tangle
+   const spaceInBlackTangle = vh - aboveTitinPerformHeight;
+   titinPerformAr[0].style.height = `${vh + spaceInBlackTangle}px`;
+};
+
+const positionBlackTangle = (blackTangle: HTMLElement, slantAglRad: number) => {
+   blackTangle.style.transform = `rotate(${slantAglRad}rad)`;
 };
 
 const resizeHandler = (
    redSlant: HTMLElement,
    lukePhoto: HTMLElement,
    slogan: HTMLElement,
+   titinPerformAr: HTMLElement[],
+   blackTangle: HTMLElement,
 ) => {
-   let [vw, vh] = getVwVh();
-   let slantAglRad = rotateRedSlant(redSlant, vw, vh);
-   let lukeOffset = positionLuke(lukePhoto, vw, vh, slantAglRad);
-   positionSlogan(slogan, lukeOffset);
+   const [vw, vh] = getVwVh();
+   const slantAglRad = rotateRedSlant(redSlant, vw, vh);
+   // const lukeOffset = positionLuke(lukePhoto, vw, vh, slantAglRad);
+   positionLuke(lukePhoto, vw, vh, slantAglRad);
+   positionSlogan(slogan, vw, vh, slantAglRad);
+   positionTitinPerform(titinPerformAr, vh, slantAglRad);
+   positionBlackTangle(blackTangle, slantAglRad);
 };
 
 function App() {
    var redSlant: HTMLElement;
    var lukePhoto: HTMLElement;
    var slogan: HTMLElement;
+   var titinPerformAr: HTMLElement[] = new Array(3); //0 is wrapper, 1 is above, 2 is text
+   var blackTangle: HTMLElement;
 
    useEffect(() => {
       //execute all useEffect promises in parallel
@@ -97,28 +145,59 @@ function App() {
          waitForElm("#slogan").then((sloganEl: HTMLElement | null) => {
             slogan = sloganEl!;
          }),
+         waitForElm("#black-tangle").then(
+            (blackTangleEl: HTMLElement | null) => {
+               blackTangle = blackTangleEl!;
+            },
+         ),
+         waitForElm("#titin-perform-wrapper").then((titinPerformWrapperEl) => {
+            titinPerformAr[0] = titinPerformWrapperEl!;
+            waitForElm("#above-titin-perform").then((aboveTitinPerformEl) => {
+               titinPerformAr[1] = aboveTitinPerformEl!;
+               waitForElm("#titin-perform").then((titinPerformEl) => {
+                  titinPerformAr[2] = titinPerformEl!;
+               });
+            });
+         }),
       ]).then(() => {
-         resizeHandler(redSlant, lukePhoto, slogan);
+         resizeHandler(
+            redSlant,
+            lukePhoto,
+            slogan,
+            titinPerformAr,
+            blackTangle,
+         );
          addEventListener("resize", () => {
-            resizeHandler(redSlant, lukePhoto, slogan);
+            resizeHandler(
+               redSlant,
+               lukePhoto,
+               slogan,
+               titinPerformAr,
+               blackTangle,
+            );
          });
       });
    });
 
    return (
       <>
-         <div id="red-slant" />
+         <title>Titin Performance</title>
+
+         <div id="red-slant" className="-z-30" />
+         <div id="white-tangle" className="-z-40"></div>
+         <div id="black-tangle-frame" className="-z-50">
+            <div className="relative h-full w-full">
+               <div id="black-tangle" className=""></div>
+            </div>
+         </div>
 
          <main className="flex justify-center">
             {/* stuff in margins */}
-            <div className="w-4/6">
+            <div id="main-margin" className="w-4/6">
                {/* above the fold */}
                <div className="relative h-screen w-full">
-                  <ShadowFrame
-                     id="luke-photo"
-                     className="absolute right-0 h-96 w-96"
-                  >
-                     <div className="absolute right-0 top-4 h-3/5 fill-greenwhite">
+                  <ShadowFrame id="luke-photo" className="absolute right-0">
+                     <div className="absolute right-0 top-4 h-3/5 fill-red-600 opacity-60">
                         {TITIN}
                      </div>
                      <img
@@ -132,6 +211,22 @@ function App() {
                         <div className="font-aver">at your best.</div>
                      </div>
                   </div>
+                  <div id="titin-perform-wrapper" className="">
+                     <div id="above-titin-perform" />
+                     <div
+                        id="titin-perform"
+                        className=" sticky fill-greenwhite"
+                     >
+                        {TITINperformance}
+                     </div>
+                  </div>
+
+                  {/* <div className="titin-perform-wrapper">
+                     <div className="above-titin-perform" />
+                     <div className="titin-perform sticky fill-greenwhite">
+                        {TITINperformance}
+                     </div>
+                  </div> */}
                </div>
             </div>
          </main>
